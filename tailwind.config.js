4ac18fcd42b05e38a93c3218c994f6afe4064628
trungtan <trungtan14@gmail.com>
2024-08-27 14:51:59 +0700

    feature core commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        sm: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+        default: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        md: "3px 3px 6px rgba(0, 0, 0, 0.5)",
+        lg: "4px 4px 8px rgba(0, 0, 0, 0.5)",
+        xl: "5px 5px 10px rgba(0, 0, 0, 0.5)",
+      },
+    },
   },
-  plugins: [],
-}
-
+  screens: {
+    'xl': '1520px',
+  },
+  plugins: [require("tailwindcss-textshadow")],
+};
